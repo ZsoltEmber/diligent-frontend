2729259f7dc07de324d9cc9d40fd99103abc7715
ZsoltEmber <132936073+ZsoltEmber@users.noreply.github.com>
2024-10-26 14:12:21 +0200

    implement and styling navbar components

--- public/scripts/component/navbar.js
+++ public/scripts/component/navbar.js
@@ -2,6 +2,15 @@ import { Component } from "../general.js";
 
 
 export function createNavbar(){
-    const navbar =  new Component("nav", {id: "navbar", class: "nav"});
+
+    const homePageLink = new Component("span", {id:"homepage", class: "navbar-item home"},["Home"]).render();
+    const registerLink = new Component("span", {id:"register-link", class: "navbar-item"}, ["register"]).render();
+    const loginLink = new Component("span", {id:"login-link", class: "navbar-item"},["login"]).render();
+
+    const leftContainer = new Component("div", {id: "nav-left", class: "nav-left"},[homePageLink]).render()
+    const rightContainer = new Component("div", {id: "nav-right", class: "nav-right"},[registerLink, loginLink]).render()
+
+
+    const navbar =  new Component("nav", {id: "navbar", class: "nav"}, [leftContainer, rightContainer]);
     return navbar.render();
 }
