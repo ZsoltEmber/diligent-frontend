1619509ab63c6639a6806929192e8d8aa4b71c69
tintapolipka <109940436+tintapolipka@users.noreply.github.com>
2024-10-27 13:13:22 +0100

    product detail component implemented, waiting for Cart

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,21 +1,9 @@
 import {Component} from "./general.js";
-import StarRatings from "../src/components/star-rating/star-ratings.js";
 
+import { createForm } from "./page/loginRegisterPage.js";
 
 
 window.onload = () => {
   console.log("hello world");
-  const text = new Component("p", {}, ["Valami", "semmi"]);
-  const newComp = new Component(
-    "p",
-    { id: "component-id", class: "egyik másik harmadik" },
-    ["Szöveg", text.render()],
-    {
-      click: () => {
-        console.log("klikk");
-      },
-    }
-  );
-  console.log(newComp.render());
-  document.body.append(newComp.render());
+  document.body.append(createForm());
 };
