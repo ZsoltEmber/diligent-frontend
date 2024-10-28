0de2b997d1b99c3b9e838bc0a708e1b1be5b268c
tintapolipka <109940436+tintapolipka@users.noreply.github.com>
2024-10-28 18:20:27 +0100

    sessionStorage changed to localstorage

--- public/scripts/general.js
+++ public/scripts/general.js
@@ -1,142 +1,156 @@
-/** 
-* @param {string} tag - the tag of the html element
-* @param {object} attributes - a key-value pair, representing html attributes for expl. {id:'idOfElement'}
-* @param {Array} attributes - an array of nodes (html elements) to append to the element as children
-* @param {object} listenerObj - a key value pair, the key is the event name, value is the function to add as eventListener {'click':()=>{console.log('')}}
-*/
+/**
+ * @param {string} tag - the tag of the html element
+ * @param {object} attributes - a key-value pair, representing html attributes for expl. {id:'idOfElement'}
+ * @param {Array} attributes - an array of nodes (html elements) to append to the element as children
+ * @param {object} listenerObj - a key value pair, the key is the event name, value is the function to add as eventListener {'click':()=>{console.log('')}}
+ */
 export class Component {
-    constructor(tag = "div", attributes = {}, children = [], listenerObj) {
-      this.tag = tag;
-      this.attributes = attributes;
-      this.children = children;
-      this.listener = listenerObj; // {'click': ()=>{}}
-      this.node = this.render(); // Only for developing reasons
-    }
-
-    render() {
-      const node = document.createElement(this.tag);
-      Object.keys(this.attributes).forEach((attrKey) =>
-        node.setAttribute(attrKey, this.attributes[attrKey])
-      );
-      if (this.children && this.children.length) {
-        this.children.forEach((child) => node.append(child));
-      }
-  
-      if (this.listener) {
-        Object.keys(this.listener).forEach((eventName) =>
-          node.addEventListener(eventName, this.listener[eventName])
-        );
-      }
-  
-      return node;
-    }
+  constructor(tag = "div", attributes = {}, children = [], listenerObj) {
+    this.tag = tag;
+    this.attributes = attributes;
+    this.children = children;
+    this.listener = listenerObj; // {'click': ()=>{}}
+    this.node = this.render(); // Only for developing reasons
   }
 
-function isCartExits(){
-  // TODO : different users shoud have different carts! Maybe use username from cookies?
-  const currentCart = sessionStorage.getItem('cart')
+  render() {
+    const node = document.createElement(this.tag);
+    Object.keys(this.attributes).forEach((attrKey) =>
+      node.setAttribute(attrKey, this.attributes[attrKey])
+    );
+    if (this.children && this.children.length) {
+      this.children.forEach((child) => node.append(child));
+    }
+
+    if (this.listener) {
+      Object.keys(this.listener).forEach((eventName) =>
+        node.addEventListener(eventName, this.listener[eventName])
+      );
+    }
+
+    return node;
+  }
+}
+
+function getKeyOfUsersCart() {
+  return `cart-${localStorage.getItem("signedUserId")}`;
+}
+
+function isCartExits() {
+  const currentCart = localStorage.getItem(getKeyOfUsersCart());
   return Boolean(currentCart);
 }
 
-function setEmptyCart(){
-  // TODO : different users shoud have different carts! Maybe use username from cookies?
-  if(isCartExits()){return;}
-  else{
-    sessionStorage.setItem('cart','[]');
+function setEmptyCart() {
+  if (isCartExits()) {
+    return;
+  } else {
+    localStorage.setItem(getKeyOfUsersCart(), "[]");
   }
-} 
+}
 
 /**
-* checks if the given object is valid product object, returns boolean 
-* @param  {{id: number, title: string, price: number, description: string, category: string, image: url, rating: {rate: number,count: number} }} getProduct {id: number, title: string, price: number, description: string, category: string, image: url, rating: {rate: number,count: number} }
-* @returns {boolean} - if the given object is valid product, returns boolean
-*/
-export function isTypeProduct(getProduct){
-  if(typeof getProduct !== "object"){
-   console.error("Wrong value type! The given parameter is not the type 'object'!") 
+ * checks if the given object is valid product object, returns boolean
+ * @param  {{id: number, title: string, price: number, description: string, category: string, image: url, rating: {rate: number,count: number} }} getProduct {id: number, title: string, price: number, description: string, category: string, image: url, rating: {rate: number,count: number} }
+ * @returns {boolean} - if the given object is valid product, returns boolean
+ */
+export function isTypeProduct(getProduct) {
+  if (typeof getProduct !== "object") {
+    console.error(
+      "Wrong value type! The given parameter is not the type 'object'!"
+    );
     return false;
   }
   let isValid = true;
-  let errorMessage = '';
-  const propertysToFind = ['id', 'title', 'price', 'description', 'category', 'image', 'rating']
-  propertysToFind.forEach(key=>{
-    if(!getProduct.hasOwnProperty(key)){
-      errorMessage+= ` ${key},`;
+  let errorMessage = "";
+  const propertysToFind = [
+    "id",
+    "title",
+    "price",
+    "description",
+    "category",
+    "image",
+    "rating",
+  ];
+  propertysToFind.forEach((key) => {
+    if (!getProduct.hasOwnProperty(key)) {
+      errorMessage += ` ${key},`;
       isValid = false;
     }
-  })
-  if(errorMessage){
-    console.error("The given parameter is missing the following keys:",errorMessage);
+  });
+  if (errorMessage) {
+    console.error(
+      "The given parameter is missing the following keys:",
+      errorMessage
+    );
   }
   return isValid;
 }
 
 /**
-* Returns the content of the cart (sessionStorage) as an array of product objects 
-* @returns {{id: number, title: string, price: number, image: url, amount:number}[]} returns an array of objects from the cart.
-*/
-  export function getCartContent(){
-    // TODO : different users shoud have different carts! Maybe use username from cookies?
-    const currentCart = sessionStorage.getItem('cart')
-    if(!currentCart){
-      return [];
-    }
-    return JSON.parse(currentCart);
+ * Returns the content of the cart (localStorage) as an array of product objects
+ * @returns {{id: number, title: string, price: number, image: url, amount:number}[]} returns an array of objects from the cart.
+ */
+export function getCartContent() {
+  const currentCart = localStorage.getItem(getKeyOfUsersCart());
+  if (!currentCart) {
+    return [];
   }
+  return JSON.parse(currentCart);
+}
 
 /**
-* adds the given product object to an array of product objects in cart (sessionStorage) 
-* @param {{id: number, title: string, price: number, description: string, category: string, image: url, rating: {rate: number,count: number} } | {id: number, title: string, price: number, image: url, amount:number}} getProduct {id: number, title: string, price: number, description: string, category: string, image: url, rating: {rate: number,count: number} }
-* @param {number} newAmount - the new number of the added product.
-*/
-export function addToCart(getProduct,newAmount){
-  // TODO : different users shoud have different carts! Maybe use username from cookies?
+ * adds the given product object to an array of product objects in cart (localStorage)
+ * @param {{id: number, title: string, price: number, description: string, category: string, image: url, rating: {rate: number,count: number} } | {id: number, title: string, price: number, image: url, amount:number}} getProduct {id: number, title: string, price: number, description: string, category: string, image: url, rating: {rate: number,count: number} }
+ * @param {number} newAmount - the new number of the added product.
+ */
+export function addToCart(getProduct, newAmount) {
   const originalData = getCartContent();
-  
+
   const newCartItem = {
     id: getProduct.id,
     title: getProduct.title,
     price: getProduct.price,
     image: getProduct.image,
     amount: newAmount,
-   };
+  };
 
-   if(!isCartExits){
-    sessionStorage.setItem('cart',JSON.stringify([newCartItem]));
+  if (!isCartExits) {
+    localStorage.setItem(getKeyOfUsersCart(), JSON.stringify([newCartItem]));
     return;
- }
+  }
 
   const storedProduct = findInCartById(getProduct.id);
-  if(storedProduct !== null){
-    originalData.splice(storedProduct.index,1,newCartItem);
+  if (storedProduct !== null) {
+    originalData.splice(storedProduct.index, 1, newCartItem);
   } else {
     originalData.push(newCartItem);
   }
-  
-  sessionStorage.setItem('cart',JSON.stringify(originalData));
+
+  localStorage.setItem(getKeyOfUsersCart(), JSON.stringify(originalData));
 }
 
 /**
-* Searching for a product with a particular id, and returns an object containing the product and the index of it in Cart.
-* @param  {number} id - the id of the product to find.
-* @returns {{product:object,index:number}|null} - if the product not exists returns null, else returns an object containing the product and the index of it in Cart : {product,index}
-*/
-export function findInCartById(id){
-  // TODO : different users shoud have different carts! Maybe use username from cookies?
-  const currentCart = sessionStorage.getItem('cart')
-  if(!currentCart){
-    console.warn('The cart is empty!');
+ * Searching for a product with a particular id, and returns an object containing the product and the index of it in Cart.
+ * @param  {number} id - the id of the product to find.
+ * @returns {{product:object,index:number}|null} - if the product not exists returns null, else returns an object containing the product and the index of it in Cart : {product,index}
+ */
+export function findInCartById(id) {
+  const currentCart = localStorage.getItem(getKeyOfUsersCart());
+  if (!currentCart) {
+    console.warn("The cart is empty!");
     return null;
   }
   let foundIndex;
-  const foundProducts = JSON.parse(currentCart).filter((product,index)=>
-    {
-      if(product.id == id){ foundIndex = index;};
-      return product.id == id;
+  const foundProducts = JSON.parse(currentCart).filter((product, index) => {
+    if (product.id == id) {
+      foundIndex = index;
     }
-  );
-  if(foundProducts.length === 0){
+    return product.id == id;
+  });
+  if (foundProducts.length === 0) {
     return null;
   }
-  return {product: foundProducts[0], index: foundIndex};
+  return { product: foundProducts[0], index: foundIndex };
 }
+
