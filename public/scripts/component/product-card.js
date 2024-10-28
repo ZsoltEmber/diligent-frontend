abeb325e56fff0aad81735fba6506efdbedcaccd
ZsoltEmber <132936073+ZsoltEmber@users.noreply.github.com>
2024-10-28 15:12:05 +0100

    add buttons to product cards, minor style changes

--- public/scripts/component/product-card.js
+++ public/scripts/component/product-card.js
@@ -6,12 +6,15 @@ export function createProductCard(product) {
     const productPrice = new Component("p", { id: "product-price", class: "product-price" }, [`Price: ${product.price}$`]).render()
     const productImage = new Component("img", { id: "product-img", class: "product-img", src: product.image }).render()
     const productDescription = new Component("p", { id: "product-desc", class: "product-desc" }, [product.description]).render()
-
+    const cartBtn = new Component("button", { id: "cart-btn", class: "cart-btn" }, ["cart"]).render()
+    const favBtn = new Component("button", { id: "fav-btn", class: "fav-btn" }, ["fav"]).render()
+    
     const productDetailsContainerLeft = new Component("div", { id: "product-left-container", class: "product-left-container" }, [productImage]).render()
     const productDetailsContainerRight = new Component("div", { id: "product-right-container", class: "product-right-container" }, [productPrice, productDescription]).render()
     const productDetailsContainer = new Component("div", { id: "product-details-container", class: "product-details-container" }, [productDetailsContainerLeft, productDetailsContainerRight]).render()
-    //TODO: implement buttons
-    const productCard = new Component("div", { id: "product-card", class: "product-card" }, [productName, productDetailsContainer])
+    const buttonContainer = new Component("div", {id: "button-container", class:"button-container"}, [favBtn, cartBtn]).render()
+    const productHeaderContainer = new Component("div", { id: "product-header-container", class: "product-header-container" }, [productName, buttonContainer]).render()
+    const productCard = new Component("div", { id: "product-card", class: "product-card" }, [productHeaderContainer, productDetailsContainer])
 
 
 
