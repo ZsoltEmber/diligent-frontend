359686c5230fbf14b39662496f929a6e4481b112
ZsoltEmber <132936073+ZsoltEmber@users.noreply.github.com>
2024-10-28 16:07:17 +0100

    add buttons to product cards, minor css changes;

--- public/scripts/component/product-card.js
+++ public/scripts/component/product-card.js
@@ -6,9 +6,11 @@ export function createProductCard(product) {
     const productPrice = new Component("p", { id: "product-price", class: "product-price" }, [`Price: ${product.price}$`]).render()
     const productImage = new Component("img", { id: "product-img", class: "product-img", src: product.image }).render()
     const productDescription = new Component("p", { id: "product-desc", class: "product-desc" }, [product.description]).render()
-    const cartBtn = new Component("button", { id: "cart-btn", class: "cart-btn" }, ["cart"]).render()
-    const favBtn = new Component("button", { id: "fav-btn", class: "fav-btn" }, ["fav"]).render()
+    const cartBtn = new Component("button", { id: "cart-btn", class: "cart-btn card-btn" }, ["cart"]).render()
+    const favBtn = new Component("button", { id: "fav-btn", class: "fav-btn card-btn" }, ["fav"]).render()
     
+
+    const heart = "U+1F90D"
     const productDetailsContainerLeft = new Component("div", { id: "product-left-container", class: "product-left-container" }, [productImage]).render()
     const productDetailsContainerRight = new Component("div", { id: "product-right-container", class: "product-right-container" }, [productPrice, productDescription]).render()
     const productDetailsContainer = new Component("div", { id: "product-details-container", class: "product-details-container" }, [productDetailsContainerLeft, productDetailsContainerRight]).render()
