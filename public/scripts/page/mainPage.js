fd6887ecc2d467edaf6f2a6aa9c623b419c704b2
ZsoltEmber <132936073+ZsoltEmber@users.noreply.github.com>
2024-10-28 13:37:00 +0100

    implement product list, add to main page, add css;

--- public/scripts/page/mainPage.js
+++ public/scripts/page/mainPage.js
@@ -1,10 +1,12 @@
 import { Component } from "../general.js";
 import { createNavbar } from "../component/navbar.js";
+import { createProductList } from "../component/product-list.js";
 
-export function createMainPage(){
+export async function createMainPage(){
     const navbar = createNavbar()
     //TODO:implement list
-    const mainPage =  new Component("div", {id: "main-page", class: "main"}, [navbar])
+    const productList = await createProductList()
+    const mainPage =  new Component("div", {id: "main-page", class: "main"}, [navbar, productList])
     return mainPage.render();
 }
 
