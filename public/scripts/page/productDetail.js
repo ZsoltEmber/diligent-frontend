d681f95f4c5138d7bf6a901789615281a22d4751
tintapolipka <109940436+tintapolipka@users.noreply.github.com>
2024-10-29 10:00:19 +0100

    CartForm integrated into ProductDetail class

--- public/scripts/page/productDetail.js
+++ public/scripts/page/productDetail.js
@@ -1,12 +1,15 @@
 import { Component } from "../general.js";
-import StarRatings from '../components/star-ratings.js';
+import StarRatings from "../components/star-ratings.js";
+import CartForm from "../component/cart-form.js";
 
 export class ProductDetail {
   constructor({ id, title, price, description, category, image, rating }) {
     this.id = id;
     this.title = title;
     this.price = price;
-    this.description = description.replaceAll(/, ?/g,', ').replaceAll(/\/ ?/g,'\/ ');
+    this.description = description
+      .replaceAll(/, ?/g, ", ")
+      .replaceAll(/\/ ?/g, "/ ");
     this.category = category;
     this.imageUrl = image;
     this.rating = rating;
@@ -20,17 +23,24 @@ export class ProductDetail {
   render() {
     this.node.innerHTML = "";
     const dialogImg = new Component(
-        "img",
+      "img",
       {
         alt: this.title,
         class: "product-detail--dialog-img",
         src: this.imageUrl,
       },
       []
-        ).render();
-    const pictureContainerDialogElement = new Component('dialog',{class:"product-detail--img-container-dialog"},[
-        dialogImg
-        ],{click:()=>{pictureContainerDialogElement.close()}}).render();
+    ).render();
+    const pictureContainerDialogElement = new Component(
+      "dialog",
+      { class: "product-detail--img-container-dialog" },
+      [dialogImg],
+      {
+        click: () => {
+          pictureContainerDialogElement.close();
+        },
+      }
+    ).render();
 
     const productImg = new Component(
       "img",
@@ -45,11 +55,12 @@ export class ProductDetail {
       "button",
       { class: "product-detail--zoom-img" },
       ["🔍"],
-      {click:()=>{
-        pictureContainerDialogElement.showModal();
-      }}
+      {
+        click: () => {
+          pictureContainerDialogElement.showModal();
+        },
+      }
     ).render();
-    
 
     const productImgContainer = new Component(
       "div",
@@ -72,9 +83,13 @@ export class ProductDetail {
       [descriptionP]
     ).render();
 
-    const priceSpan = new Component("span", {
-      class: "product-detail--price-span",
-    },[this.price]).render();
+    const priceSpan = new Component(
+      "span",
+      {
+        class: "product-detail--price-span",
+      },
+      [this.price]
+    ).render();
     const priceP = new Component("p", {}, [
       "price: ",
       priceSpan,
@@ -87,18 +102,27 @@ export class ProductDetail {
       [priceP]
     ).render();
     //<button class="product-detail--add-to-cart-btn form-button">Add to Cart</button>
-    const addToCartBtn = new Component('button',{class:"product-detail--add-to-cart-btn form-button"},['Add to Cart'],
-        {click:()=>{alert('Added to Cart')}}
-    ).render();
+    const addToCartBtn = new CartForm({
+      id: this.id,
+      title: this.title,
+      price: this.price,
+      description: this.description,
+      category: this.category,
+      image: this.imageUrl,
+      rating: this.rating,
+    }).render();
 
-    const productDetailInfoContainer = new Component('div',{class:"product-detail--info-container"},
-        [
+    const productDetailInfoContainer = new Component(
+      "div",
+      { class: "product-detail--info-container" },
+      [
         titleH1,
         productRatingComponent,
         descriptionContainer,
         priceContainer,
-        addToCartBtn
-    ]).render();
+        addToCartBtn,
+      ]
+    ).render();
 
     this.node.append(
       productImgContainer,
@@ -114,7 +138,7 @@ let x = new ProductDetail({
   id: 20,
   title: "DANVOUY Womens T Shirt Casual Cotton Short",
   price: 12.99,
-  description:
+  description:a
     "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
   category: "women's clothing",
   image: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
