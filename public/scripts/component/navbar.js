e83b111971dfa05ec45769c5ee4983b2a6b8d532
tintapolipka <109940436+tintapolipka@users.noreply.github.com>
2024-10-31 14:44:35 +0100

    refactoring closed

--- public/scripts/component/navbar.js
+++ public/scripts/component/navbar.js
@@ -1,12 +1,12 @@
 import { Component } from "../general.js";
 import { createForm } from "../page/loginRegisterPage.js";
-
+import { getCartContent } from "../cart.js";
 
 export function createNavbar() {
     const homePageLink = new Component("span", { id: "homepage", class: "navbar-item home" }, ["Home"]).render();
     const registerLink = new Component("span", { id: "register-link", class: "navbar-item" }, ["register"], { click: () => handleClick("register") }).render();
     const loginLink = new Component("span", { id: "login-link", class: "navbar-item" }, ["login"], { click: () => handleClick("login") }).render();
-    const cartLink = new Component("img", { id: "cart-icon", style:"margin-top:7px; margin-right:1rem", class: "cart-btn card-btn", src: "../../styles/resource/shopping-cart-white.png", alt: "cart-icon" }, {click: ()=>handleAddToCart()}).render();
+    const cartLink = new Component("img", { id: "cart-icon", style:"margin-top:7px; margin-right:1rem", class: "cart-btn card-btn", src: chooseCartImage(), alt: "cart-icon" }, {click: ()=>handleAddToCart()}).render();
 
     const leftContainer = new Component("div", { id: "nav-left", class: "nav-left" }, [homePageLink]).render()
     const rightContainer = new Component("div", { id: "nav-right", class: "nav-right" }, [registerLink, loginLink,cartLink]).render()
@@ -23,5 +23,6 @@ function handleClick(formType) {
 }
 
 function chooseCartImage(){
-
+   return getCartContent().length === 0? "../../styles/resource/shopping-cart-white.png":"../../styles/resource/shopping-cart-full.png"
 }
+console.log(chooseCartImage()) ;
