76ca1b12823913d517aee5a04ed8c04d880b1d04
tintapolipka <109940436+tintapolipka@users.noreply.github.com>
2024-10-31 13:54:23 +0100

    cart-button added to navBar

--- public/scripts/component/navbar.js
+++ public/scripts/component/navbar.js
@@ -6,9 +6,10 @@ export function createNavbar() {
     const homePageLink = new Component("span", { id: "homepage", class: "navbar-item home" }, ["Home"]).render();
     const registerLink = new Component("span", { id: "register-link", class: "navbar-item" }, ["register"], { click: () => handleClick("register") }).render();
     const loginLink = new Component("span", { id: "login-link", class: "navbar-item" }, ["login"], { click: () => handleClick("login") }).render();
+    const cartLink = new Component("img", { id: "cart-icon", style:"margin-top:7px; margin-right:1rem", class: "cart-btn card-btn", src: "../../styles/resource/shopping-cart-white.png", alt: "cart-icon" }, {click: ()=>handleAddToCart()}).render();
 
     const leftContainer = new Component("div", { id: "nav-left", class: "nav-left" }, [homePageLink]).render()
-    const rightContainer = new Component("div", { id: "nav-right", class: "nav-right" }, [registerLink, loginLink]).render()
+    const rightContainer = new Component("div", { id: "nav-right", class: "nav-right" }, [registerLink, loginLink,cartLink]).render()
 
 
     const navbar = new Component("nav", { id: "navbar", class: "nav" }, [leftContainer, rightContainer]);
@@ -19,4 +20,8 @@ function handleClick(formType) {
     const root = document.getElementById("root")
     root.innerHTML = "";
     root.append(createForm(formType))
+}
+
+function chooseCartImage(){
+
 }
