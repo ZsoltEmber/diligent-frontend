2865571a4a9c70678cdc3b7606c29eb63eb60371
tintapolipka <109940436+tintapolipka@users.noreply.github.com>
2024-10-31 15:13:38 +0100

    link to Cart added to navbar

--- public/scripts/component/navbar.js
+++ public/scripts/component/navbar.js
@@ -1,12 +1,14 @@
 import { Component } from "../general.js";
 import { createForm } from "../page/loginRegisterPage.js";
 import { getCartContent } from "../cart.js";
+import { createMainPage } from "../page/mainPage.js";
+import { createCartPage } from "../page/cartPage.js";
 
 export function createNavbar() {
-    const homePageLink = new Component("span", { id: "homepage", class: "navbar-item home" }, ["Home"]).render();
+    const homePageLink = new Component("span", { id: "homepage", class: "navbar-item home" }, ["Home"],{click:handleClickHome}).render();
     const registerLink = new Component("span", { id: "register-link", class: "navbar-item" }, ["register"], { click: () => handleClick("register") }).render();
     const loginLink = new Component("span", { id: "login-link", class: "navbar-item" }, ["login"], { click: () => handleClick("login") }).render();
-    const cartLink = new Component("img", { id: "cart-icon", style:"margin-top:7px; margin-right:1rem", class: "cart-btn card-btn", src: chooseCartImage(), alt: "cart-icon" }, {click: ()=>handleAddToCart()}).render();
+    const cartLink = new Component("img", { id: "cart-icon", style:"margin-top:7px; margin-right:1rem", class: "cart-btn card-btn", src: chooseCartImage(), alt: "cart-icon" },[],{click: handleClickCart}).render();
 
     const leftContainer = new Component("div", { id: "nav-left", class: "nav-left" }, [homePageLink]).render()
     const rightContainer = new Component("div", { id: "nav-right", class: "nav-right" }, [registerLink, loginLink,cartLink]).render()
@@ -20,6 +22,19 @@ function handleClick(formType) {
     const root = document.getElementById("root")
     root.innerHTML = "";
     root.append(createForm(formType))
+}
+
+function handleClickCart() {
+    const root = document.getElementById("root")
+    root.innerHTML = "";
+    root.append(createCartPage());
+}
+
+const handleClickHome = async () => {
+    const root = document.getElementById("root");
+    root.innerHTML = "";
+  const mainPage = await createMainPage()
+  root.append(mainPage)
 }
 
 function chooseCartImage(){
